5f6165446c0b63a4c79136367692ff241a36722d
Justin Trantham <takescake@outlook.com>
2026-07-31 16:08:12 -0500

    Add public SDK entrypoint

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export * from "./schema/page.js";
+export * from "./protocol/meaning.js";
+export * from "./encoding/pagePipeline.js";
+export * from "./transport/adapters.js";
+export * from "./adapters/index.js";
